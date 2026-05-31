0a6c3ce2b2145aabd15c99957043ba7fbd422092
mearvk <mearvk@outlook.com>
2026-05-31 15:25:56 -0400

    System Touch

--- source/Main.java
+++ source/Main.java
@@ -14,57 +14,53 @@ public class Main
 
     protected static final Integer WEB_EXPRESS_SERVER_SOCKET = 49152;
 
-    protected static final String WEB_EXPRESS_SERVER_THREAD_NAME = "WEB_EXPRESS_TELNET_PROXY_SERVER";
+    protected static final String WEB_EXPRESS_SERVER_THREAD_NAME = "WEBEXPRESS_TELNET_PROXY_SERVER";
 
     protected static final Integer AES2_WEBEXPRESS_SERVER_SOCKET = 5512;
 
-    protected static final String AES2_WEBEXPRESS_SERVER_THREAD_NAME = "WEB_EXPRESS_AES2_SERVER";
+    protected static final String AES2_WEBEXPRESS_SERVER_THREAD_NAME = "WEBEXPRESS_AES2_SERVER";
 
     protected static final Integer BITCOIN_WEBEXPRESS_SERVER_SOCKET = 6682;
 
-    protected static final String BITCOIN_WEBEXPRESS_SERVER_THREAD_NAME = "WEB_EXPRESS_BITCOIN_SERVER";
+    protected static final String BITCOIN_WEBEXPRESS_SERVER_THREAD_NAME = "WEBEXPRESS_BITCOIN_SERVER";
 
-    protected static final String WEB_EXPRESS_HOST = "localhost";
+    protected static final String WEBEXPRESS_HOST = "localhost";
 
-    protected static final String AES_WEBEXPRESS = "localhost";
+    protected static final String AES_WEBEXPRESS_HOST = "localhost";
 
-    protected static final String BITCOIN_WEBEXPRESS = "localhost";
+    protected static final String BITCOIN_WEBEXPRESS_HOST = "localhost";
 
     public Main()
     {
         System.out.println();
 
-        System.out.println("[ Java National Finance Engine v.28.1.1 Software Processes Starting ]");
+            System.out.println("[ Java National Finance Engine v.28.1.1 Software Processes Starting ]");
 
-        System.out.println(". Cryptography/Cryptology AES2 National Cryptolograph Enabled DSS 5.0 .");
+            System.out.println(". Cryptography/Cryptology AES2 National Cryptolograph Enabled DSS 5.0 .");
 
-        System.out.println(". Bitcoin Lightweight Binary Trader 2.0 Enabled ₿ Running on Bitcoin Open-Source v24.0 or newer .");
+            System.out.println(". Bitcoin Lightweight Binary Trader 2.0 Enabled ₿ Running on Bitcoin Open-Source v24.0 or newer .");
 
-        System.out.println(". Operating within and United to National Authority of US United States and State of California in Coalition of and for North Carolina her betterment .");
+            System.out.println(". Operating within and United to National Authority of US United States and State of California in Coalition of and for North Carolina her betterment .");
 
         System.out.println();
 
-        //
+            CommonRails.printSystemComponent(this, this.hashCode(),". Java™ National Finance Engine v.2811.1 v.11.1 .");
 
-        CommonRails.printSystemComponent(this, this.hashCode(),". Java™ National Finance Engine v.2811.1 v.11.1 .");
-
-        CommonRails.printSystemComponent(this, this.hashCode(),". National NitroExpress™ Web Engine Starting .");
-
-        //
+            CommonRails.printSystemComponent(this, this.hashCode(),". National NitroExpress™ Web Engine Starting .");
 
         NitroWebExpress nitro = new NitroWebExpress();
 
-          nitro.PORT = 49152;
+            nitro.PORT = 49152;
 
-          nitro.HOST = "localhost";
+            nitro.HOST = "localhost";
 
-          nitro.THREAD_NAME = "United States::D500::WebExpress";
+            nitro.THREAD_NAME = "United States::D500::WebExpress";
 
-          nitro.TELNET_PROXY_ENABLED = Boolean.TRUE;
+            nitro.TELNET_PROXY_ENABLED = Boolean.TRUE;
 
-          nitro.bridge.AES_COMPONENT = new NitroWebExpress.Aspect.AESCompliant(AES_WEBEXPRESS, AES2_WEBEXPRESS_SERVER_SOCKET, AES2_WEBEXPRESS_SERVER_THREAD_NAME, Boolean.TRUE);
+            nitro.bridge.AES_COMPONENT = new NitroWebExpress.Aspect.AESCompliant(AES_WEBEXPRESS_HOST, AES2_WEBEXPRESS_SERVER_SOCKET, AES2_WEBEXPRESS_SERVER_THREAD_NAME, Boolean.TRUE);
 
-          nitro.bridge.BITCOIN_COMPONENT = new NitroWebExpress.Aspect.BitcoinCompliant(BITCOIN_WEBEXPRESS, BITCOIN_WEBEXPRESS_SERVER_SOCKET, BITCOIN_WEBEXPRESS_SERVER_THREAD_NAME, Boolean.TRUE);
+            nitro.bridge.BITCOIN_COMPONENT = new NitroWebExpress.Aspect.BitcoinCompliant(BITCOIN_WEBEXPRESS_HOST, BITCOIN_WEBEXPRESS_SERVER_SOCKET, BITCOIN_WEBEXPRESS_SERVER_THREAD_NAME, Boolean.TRUE);
 
         nitro.start();
     }
