ef17ec08c682a4206cc9217d7be047a681d3a68d
mearvk <mearvk@outlook.com>
2026-06-03 19:23:19 -0400

    System Touch

--- linux/c/international_common.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "international_common.h"
-
-void InternationalCommonRails_IranianWedding_printSystemComponent(const char *context)
-{
-    printf("[InternationalCommonRails::IranianWedding] System component context: %s\n", context ? context : "(null)");
-}
